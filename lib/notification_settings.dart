b3a607417b06ded3f277319f42fd6e3127bf3663
Explorador412 <jin432101@gmail.com>
2024-11-13 01:22:51 +0900

    Initial Flutter project setup

---
+++ lib/notification_settings.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NotificationSettingsPage(),
+    );
+  }
+}
+
+class NotificationSettingsPage extends StatefulWidget {
+  @override
+  _NotificationSettingsPageState createState() => _NotificationSettingsPageState();
+}
+
+class _NotificationSettingsPageState extends State<NotificationSettingsPage> {
+  bool spendingWarning = true;
+  bool regularSpending = true;
+  bool roomInvitation = true;
+  int selectedDay = 17;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        leading: const BackButton(
+          color: Colors.black,
+        ),
+        title: Text(
+          '알림 설정',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.grey[100],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            SizedBox(height: 20.0),
+            // 지출 경고 알림
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('지출 경고 알림', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Switch(
+                  value: spendingWarning,
+                  onChanged: (val) => setState(() => spendingWarning = val),
+                  activeColor: Colors.green,
+                ),
+              ],
+            ),
+            Divider(),
+            // 정기 지출 알림
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('정기 지출 알림', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Switch(
+                  value: regularSpending,
+                  onChanged: (val) => setState(() => regularSpending = val),
+                  activeColor: Colors.green,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Icon(Icons.add, color: Colors.blue),
+                  ),
+                  onPressed: () {
+                    //버튼 클릭 시 추가
+                  },
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'NETFLIX',
+                      hintStyle: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Text('매월', style: TextStyle(fontSize: 16)),
+                SizedBox(width: 10),
+                DropdownButton<int>(
+                  value: selectedDay,
+                  items: List.generate(31, (index) => index + 1)
+                      .map((day) => DropdownMenuItem<int>(
+                    value: day,
+                    child: Text(day.toString()),
+                  ))
+                      .toList(),
+                  onChanged: (day) {
+                    setState(() {
+                      selectedDay = day!;
+                    });
+                  },
+                ),
+                Text('일', style: TextStyle(fontSize: 16)),
+                IconButton(
+                  icon: Icon(Icons.delete, color: Colors.grey),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Divider(),
+
+            // 방 초대 알림
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('방 초대 알림', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    Switch(
+                      value: roomInvitation,
+                      onChanged: (val) => setState(() => roomInvitation = val),
+                      activeColor: Colors.green,
+                    ),
+                  ],
+                ),
+                Text('공유 가계부 초대 알림이 옵니다', style: TextStyle(color: Colors.grey[600])),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: '내역'),
+          BottomNavigationBarItem(icon: Icon(Icons.pie_chart), label: '그래프'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet), label: '예산 관리'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '설정'),
+        ],
+        currentIndex: 3,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
+
---
+++ lib/share_settings.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SharingSettingsPage(),
+    );
+  }
+}
+
+
+class SharingSettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        leading: const BackButton(
+          color: Colors.black,
+        ),
+        title: Text(
+          '공유 설정',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20.0),
+            Row(
+              children: [
+                SizedBox(width: 15.0),
+                Text(
+                  '방 이름 설정',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 10.0),
+                SizedBox(
+                  height: 25.0, width: 60.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      '수정',
+                      style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold), // Smaller font size
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      foregroundColor: Colors.blue[700],
+                      backgroundColor: Colors.blue[100],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              width: 200.0, height: 50.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[300],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: 'Enter',
+                  hintStyle: TextStyle(color: Colors.grey[600]),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.chat_bubble, color: Colors.black),
+              title: Text(
+                '카카오톡으로 친구 초대',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onTap: () {},
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.person_add, color: Colors.black),
+              title: Text(
+                '아이디로 친구 추가',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onTap: () {},
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: '내역'),
+          BottomNavigationBarItem(icon: Icon(Icons.pie_chart), label: '그래프'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet), label: '예산 관리'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '설정'),
+        ],
+        currentIndex: 3,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
